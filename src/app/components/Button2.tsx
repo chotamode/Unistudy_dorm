1a99bca90ca990b056d8c2abbac86199f2c67484
Aleksandr Drannikov <dsany014789@gmail.com>
2024-09-25 01:51:39 +0200

    Header

---
+++ src/app/components/Button2.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Button2Props {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Button2 = ({
+                     children,
+                    className,
+                 }: Button2Props) => {
+    return (
+        <button
+            className={`px-8 py-3 rounded-xl text-xl inline-flex items-center justify-center bg-[#0F478D] text-white font-semibold ${className}`}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button2;

5b49b2f3086a90f3025e18b26fbc3224fea73515
Aleksandr Drannikov <dsany014789@gmail.com>
2024-09-24 18:55:17 +0200

    Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,26 +1,35 @@
 import Image from 'next/image';
 import logo from '../../../public/logo.svg';
-import vkLogo from '../../../public/social_media/vk_logo.svg';
 import fbLogo from '../../../public/social_media/fb_logo.svg';
 import instaLogo from '../../../public/social_media/inst_logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="bg-[#D9E8FB] p-4 mt-auto text-black flex flex-row justify-between items-center px-64">
-            <Image src={logo} alt="Logo" className="mb-4" width={150} height={150}/>
-            <div className="text-center">
-                <ul className="flex flex-col text-left font-montserrat">
-                    <li><a href="#contacts" className="hover:underline">Контакты</a></li>
-                    <li><a href="#room-selection" className="hover:underline">Выбор комнаты</a></li>
-                    <li><a href="#booking" className="hover:underline">Перейти к бронированию</a></li>
-                </ul>
+        <footer className="bg-[#D9E8FB] p-4 mt-auto text-black flex flex-shrink flex-row justify-between items-center px-44">
+            <div className="flex justify-start items-center mr-10 ">
+                <Image src={logo} alt="Logo" className="mb-4 mr-20 " width={225} height={225}/>
+
+                <div className="text-center font-medium">
+                    <ul className="flex flex-col text-left font-montserrat gap-y-2 ">
+                        <li className="text-xl">
+                            <div><a href="#booking" className="hover:underline">To book</a></div>
+                            <div><a href="#contacts" className="hover:underline">Contacts</a></div>
+                        </li>
+                        <li className="text-base ">
+                            <div>Correspondence address:</div>
+                            <div><a href="#adress" className="hover:underline">Jaurisova 515/4, Praha 4 - Michle</a></div>
+                        </li>
+                        <li className="text-base"><a href="#https://roomrent.cz/" className="hover:underline">Link to our old website</a></li>
+                    </ul>
+                </div>
             </div>
-            <div className={"flex flex-col text-center"}>
-                <p>+420 999 99 99</p>
-                <div className={"flex flex-row justify-center"}>
-                    <Image src={vkLogo} alt="VK" width={30} height={30}/>
-                    <Image src={fbLogo} alt="Facebook" width={30} height={30}/>
+
+            <div className={"flex flex-col text-2xl text-center font-medium"}>
+                <p>+420 775 095 879</p>
+                <p>+420 773 037 688</p>
+                <div className={"flex flex-row justify-end gap-x-2"}>
                     <Image src={instaLogo} alt="Instagram" width={30} height={30}/>
+                    <Image src={fbLogo} alt="Facebook" width={30} height={30}/>
                 </div>
             </div>
         </footer>

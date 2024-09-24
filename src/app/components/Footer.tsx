c1e150d7952b0ba44a128f50552d350435e2829a
Aleksandr Drannikov <dsany014789@gmail.com>
2024-09-24 19:22:19 +0200

    Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ import instaLogo from '../../../public/social_media/inst_logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="bg-[#D9E8FB] p-4 mt-auto text-black flex flex-shrink flex-row justify-between items-center px-44">
+        <footer className="bg-[#D9E8FB] p-4 mt-auto text-black flex flex-shrink flex-row justify-between items-center px-44 rounded-large">
             <div className="flex justify-start items-center mr-10 ">
                 <Image src={logo} alt="Logo" className="mb-4 mr-20 " width={225} height={225}/>
 
@@ -15,16 +15,16 @@ const Footer = () => {
                             <div><a href="#booking" className="hover:underline">To book</a></div>
                             <div><a href="#contacts" className="hover:underline">Contacts</a></div>
                         </li>
-                        <li className="text-base ">
+                        <li className="text-lg">
                             <div>Correspondence address:</div>
                             <div><a href="#adress" className="hover:underline">Jaurisova 515/4, Praha 4 - Michle</a></div>
                         </li>
-                        <li className="text-base"><a href="#https://roomrent.cz/" className="hover:underline">Link to our old website</a></li>
+                        <li className="text-lg"><a href="#https://roomrent.cz/" className="hover:underline">Link to our old website</a></li>
                     </ul>
                 </div>
             </div>
 
-            <div className={"flex flex-col text-2xl text-center font-medium"}>
+            <div className={"flex flex-col text-xxl text-center font-medium"}>
                 <p>+420 775 095 879</p>
                 <p>+420 773 037 688</p>
                 <div className={"flex flex-row justify-end gap-x-2"}>

d12dc5cb39d7b2b671d104b1018f344a3bf38d5d
Aleksandr Drannikov <dsany014789@gmail.com>
2024-09-24 15:09:56 +0200

    Start

--- src/app/components/RoomCard.tsx
+++ src/app/components/RoomCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from "@/app/components/Button";
 import {white} from "next/dist/lib/picocolors";
+import Link from 'next/link';
 
 interface RoomCardProps {
     name: string;
@@ -17,7 +18,9 @@ const RoomCard: React.FC<RoomCardProps> = ({name, address, description, price_mo
                 <img src={image} alt={name} className="w-full h-48 object-cover mb-2"/>
                 <h2 className="text-xl font-bold mb-2">{name}</h2>
                 <p>Только для мальчиков</p>
-                <Button backgroundColor={"bg-white"} textColor={"text-black"} textSize={"sm"}>Узнать подробнее</Button>
+                <Link href={"../rooms"}>
+                    <Button backgroundColor={"bg-white"} textColor={"text-black"} textSize={"sm"}>Узнать подробнее</Button>
+                </Link>
                 {/*<p className="text-gray-700 mb-2">{address}</p>*/}
                 {/*<p className="text-gray-700 mb-4">{description}</p>*/}
                 {/*<p className="text-left text-lg font-semibold text-blue-600">${price_month}/month</p>*/}

9e3433bc4efcddc8f78136aa10f30bbe51b3e96f
Aleksandr Drannikov <dsany014789@gmail.com>
2024-09-30 18:02:23 +0200

    upper text

--- src/app/MainPage.tsx
+++ src/app/MainPage.tsx
@@ -36,13 +36,12 @@ const MainPage = () => {
                         objectFit="cover"
                         width={3840}
                         height={2160}
-
                    />
                 <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center">
                     <h1 className="text-1xl phonexs:text-2xl phone:text-3xl md:text-5xl lg:text-[4.2rem] xl:text-[5rem] font-bold text-black p-4 sm:p-12 md:p-16 rounded-xl text-center">
                         <div className="text-white">Student housing rentals</div>
                         <div className="text-[#A6D0FF]"> — easy and convenient!</div>
-                        <p className="hidden sm:block text-white mt-2 text-xs sm:text-xs md:text-sm lg:text-lg font-light sm:w-2/3 md:w-1/2 mx-auto">
+                        <p className="hidden sm:block text-white mt-2 text-xs sm:text-xs md:text-sm lg:text-xl font-light sm:w-2/3 md:w-1/2 mx-auto">
                             We understand how important it is for students to find comfortable and affordable housing,
                             so we have created a platform that helps you quickly and efficiently choose an apartment
                             that is right for you.

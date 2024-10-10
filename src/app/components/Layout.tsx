96f74c8fb473817414b53e02fa7f9f4646bbbf47
Eduard <chotamode@gmail.com>
2024-10-10 23:53:15 +0200

    scaling not work rn

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from './Footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
+    <div className="min-h-screen bg-gray-100 flex flex-col h-screen">
       <Header />
       <main className="flex-grow pb-28">
         {children}

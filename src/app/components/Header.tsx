8e65cb861112759877f9eb7baa349a0e3c7b030a
Winston Laptop <amirkass84@gmail.com>
2024-10-31 19:33:45 +0100

    fixed a lot of things

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,21 +1,29 @@
+"use client";
+
 import Image from 'next/image';
 import logo from '../../../public/logo.svg';
 import Button2 from "@/app/components/Button2";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+    const pathname = usePathname();
+    const isBlueBackground = /^\/rooms\/\d+\/reservation\/\d+$/.test(pathname);
+    const isGayBlueBackground = pathname.includes('/rooms') && pathname.includes('/reservation') && pathname.includes('/end');
+
     return (
-        <header className="bg-none text-white py-3 tablet:py-8 px-5 tablet:px-20">
-            <nav className="mt-2 flex flex-row justify-between items-center font-montserrat w-full">
-                {/*  logo*/}
-                <div className="relative w-10 h-10 ml-2 phone:-ml-6 md:ml-0 tablet:w-14 tablet:h-14 flex-shrink-0">
+        <header
+            className={`static top-0 left-0 w-full p-4 ${isGayBlueBackground ? 'bg-blue-100 tablet:bg-transparent' : isBlueBackground ? 'bg-[#0F478D] md:bg-transparent' : 'bg-transparent'} z-20`}>
+            <nav className="flex flex-row justify-between items-center font-montserrat w-full">
+                {/* Logo */}
+                <div className="relative w-10 h-10 tablet:w-14 tablet:h-14 flex-shrink-0">
                     <Link href="/">
-                        <Image src={logo} alt="Logo" fill />
+                        <Image src={logo} alt="Logo" fill/>
                     </Link>
                 </div>
                 <div className={" text-sm tablet:text-bold font-medium"}>
                     <Link href="/contacts">
-                        <div className="text-black cursor-pointer">Contacts</div>
+                        <div className="text-white md:text-black cursor-pointer">Contacts</div>
                     </Link>
                 </div>
             </nav>
--- src/app/rooms/[id]/reservation/[bedID]/end/page.tsx
+++ src/app/rooms/[id]/reservation/[bedID]/end/page.tsx
@@ -6,19 +6,19 @@ import geoIcon from '@/assets/geoIcon.svg';
 const stage4Page = () => {
     return (
         <Layout>
-            <div className={"flex flex-col justify-center items-center h-[47rem] bg-blue-100 rounded-3xl mx-20"}>
-                <div className={"flex flex-col justify-center items-center gap-2 mb-10"}>
-                    <h1 className={"text-7xl font-semibold"}>Now the manager will</h1>
-                    <h1 className={"text-7xl font-semibold"}>contact you</h1>
+            <div className={"flex flex-col justify-center items-center h-[47rem] bg-blue-100 rounded-0 tablet:rounded-3xl mx-0 sm:mx-10 md:mx-20 "}>
+                <div className={"flex flex-col justify-center items-center gap-2 mb-6 md:mb-10"}>
+                    <h1 className={"zfold:text-2xl phone:text-4xl md:text-5xl laptop:text-7xl font-semibold text-center"}>Now the manager will</h1>
+                    <h1 className={"zfold:text-2xl phone:text-4xl md:text-5xl laptop:text-7xl font-semibold text-center"}>contact you</h1>
                 </div>
-                <div className={"flex items-center gap-2"}>
-                    <Image src={geoIcon} alt="Geo Icon"/>
-                    <div className={"bg-[#0F478D] rounded-3xl py-4 px-12 mb-2"}>
-                        <p className={"text-white"}>Kirovogradskaya street, 13A</p>
+                <div className={"flex items-center pr-8 laptop:pr-16 laptop:mr-1 gap-0 laptop:gap-2"}>
+                    <Image src={geoIcon} alt="Geo Icon" className="w-10 h-10 laptop:w-16 laptop:h-16"/>
+                    <div className={"bg-[#0F478D] rounded-3xl  py-4 px-8 md:px-12 mb-0 md:mb-0"}>
+                        <p className={"text-white phonese:text-sm zfold:text-xs laptop:text-base"}>Kirovogradskaya street, 13A</p>
                     </div>
                 </div>
-                <p className={"underline mb-5"}>Downloading documents</p>
-                <div className={"pr-11"}>
+                <p className={"underline mb-5 zfold:text-sm"}>Downloading documents</p>
+                <div className={"pr-0 laptop:pr-0 zfold:text-xs phonese:text-base"}>
                     <GoToMainPageButton text="Go back to the main page"/>
                 </div>
             </div>

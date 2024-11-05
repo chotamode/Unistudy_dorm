870bda85a9efbe65450fe6f4297578d092130d15
Eduard <chotamode@gmail.com>
2024-11-06 00:24:11 +0100

    git merge fun

--- src/app/components/RoomCard.tsx
+++ src/app/components/RoomCard.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Button2 from "@/app/components/Button2";
 import Link from 'next/link';
 import Image from "next/image";
 import arrowButton from "../../assets/arrow_button.svg";
-import { getFirstRoomPhoto } from '@/app/api/rooms';
+import {getFirstRoomPhoto} from '@/app/api/rooms';
 import {ReservationContextProvider} from "@/app/context/ReservationContext";
 
 interface RoomCardProps {
@@ -19,16 +19,16 @@ interface RoomCardProps {
 }
 
 const RoomCard: React.FC<RoomCardProps> = ({
-    id = 1,
-    name = "Default Room Name",
-    address = "Default Address",
-    sex = "Girls only",
-    apart_name = "Penthouse Apartment",
-    background = "/images/rommbg.png",
-    floor = "Fifth Floor",
-    gender = 'both',
-    year = new Date().getFullYear(),
-}) => {
+                                               id = 1,
+                                               name = "Default Room Name",
+                                               address = "Default Address",
+                                               sex = "Girls only",
+                                               apart_name = "Penthouse Apartment",
+                                               background = "/images/rommbg.png",
+                                               floor = "Fifth Floor",
+                                               gender = 'both',
+                                               year = new Date().getFullYear(),
+                                           }) => {
     const [firstPhoto, setFirstPhoto] = useState<string>(background);
 
     useEffect(() => {
@@ -48,15 +48,16 @@ const RoomCard: React.FC<RoomCardProps> = ({
         <div className="mb-10">
             <div
                 className="hidden rounded-xxl md:flex flex-col justify-end overflow-hidden
-                           bg-cover bg-center min-h-120 w-110 filter grayscale-[1%]
+                           bg-cover bg-center min-h-120 w-110 filter grayscale-[1%] h-[30rem]
                            desktop:min-h-[30rem] desktop:w-[26rem]
                            medium-desktop:min-h-120 medium-desktop:w-110
                            large-desktop:min-h-[37rem] large-desktop:w-[32rem]"
-                style={{ backgroundImage: `url(${firstPhoto})` }}
+                style={{backgroundImage: `url(${firstPhoto})`}}
             >
-                <Link href={`../rooms/${id}`}>
+                <Link className={"relative w-full h-full"}
+                    href={`../rooms/${id}`}>
                     <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
-                    <div className="px-4 text-white mb-8 filter z-10 relative font-semibold">
+                    <div className="px-4 text-white filter z-10 relative font-semibold w-full h-full flex flex-col justify-end pb-6">
                         <h2 className="text-xxl font-black mb-2">{name}</h2>
                         <div className="text-adxs my-4">
                             <p>{floor} | {apart_name}</p>
@@ -72,10 +73,12 @@ const RoomCard: React.FC<RoomCardProps> = ({
             >
                 <Link href={`../rooms/${id}`}>
                     <div className="flex bg-cover bg-no-repeat bg-center rounded-xl
-                                my-2 bg-clip-content w-[22rem] h-[13.5rem]"
-                         style={{ backgroundImage: `url(${firstPhoto})` }}
-                    ></div>
-                    <div className="flex flex-row justify-between items-end w-[368px] px-2 text-black mb-2 filter font-semibold">
+            my-2 bg-clip-content w-[22rem] h-[13.5rem]"
+                         style={{backgroundImage: `url(${firstPhoto})`}}
+                    >
+                    </div>
+                    <div
+                        className="flex flex-row justify-between items-end w-[368px] px-2 text-black mb-2 filter font-semibold">
                         <div className="flex flex-col">
                             <h2 className="text-xxl font-black mb-2">{name}</h2>
                         </div>

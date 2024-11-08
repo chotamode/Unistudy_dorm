d59937f07878d6d56496a9dfde29c346232ba910
Winston Laptop <amirkass84@gmail.com>
2024-11-08 12:00:29 +0100

    fixed "go back" button. Now leads to a previous page depending on the dorm

--- src/app/[dorm]/rooms/[id]/reservation/[bedID]/end/page.tsx
+++ src/app/[dorm]/rooms/[id]/reservation/[bedID]/end/page.tsx
@@ -65,17 +65,25 @@ const Stage4Page = () => {
     return (
         <Layout>
             <div className={"flex flex-col justify-center items-center h-[47rem] bg-blue-100 rounded-0 tablet:rounded-3xl mx-0 sm:mx-10 md:mx-20 "}>
+
+                {/*контейнер с надписью */}
                 <div className={"flex flex-col justify-center items-center gap-2 mb-6 md:mb-10"}>
                     <p className={"zfold:text-3xl phone:text-4xl md:text-5xl laptop:text-6xl font-semibold text-center"}>Thank you! Our manager </p>
                     <p className={"zfold:text-3xl phone:text-4xl md:text-5xl laptop:text-6xl font-semibold text-center"}>will contact you shortly.</p>
                 </div>
+
+                    {/*контейнер с иконкой и адресом */}
                 <div className={"flex flex-row items-center pr-8 laptop:pr-16 laptop:mr-1 gap- laptop:gap-2"}>
                     <Image src={geoIcon} alt="Geo Icon" className="w-10 h-10 laptop:w-16 laptop:h-16"/>
                     <div className={"bg-[#0F478D] rounded-3xl  py-4 px-8 md:px-12 mb-0 md:mb-0"}>
                         <p className={"text-white phonese:text-sm zfold:text-xs laptop:text-base"}>{room.address}</p>
                     </div>
                 </div>
+
+                {/*кнопка скачать документы*/}
                 <p className={"underline mb-5 zfold:text-sm"} onClick={generatePDF}>Downloading documents</p>
+
+                {/*кнопка назад*/}
                 <div className={"pr-0 laptop:pr-0 zfold:text-xs phonese:text-base"}>
                     <GoToMainPageButton text="Go back to the main page"/>
                 </div>

09c3fdc0f1765f3c60f7d624233793b4fa2883ff
Eduard <chotamode@gmail.com>
2024-11-22 01:45:09 +0100

    fix telephone

--- src/app/[dorm]/rooms/[id]/page.tsx
+++ src/app/[dorm]/rooms/[id]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
-import { useParams, useSearchParams } from 'next/navigation';
-import { getRoomById, getBedsByRoomId, getRoomDetailsByRoomId, getRoomType, getRoomImages } from '../../../api/rooms';
+import React, {useEffect, useState} from 'react';
+import {useParams, useSearchParams} from 'next/navigation';
+import {getRoomById, getBedsByRoomId, getRoomDetailsByRoomId, getRoomType, getRoomImages} from '../../../api/rooms';
 import Image from 'next/image';
 import Layout from "@/app/components/Layout";
 import placeholderImage from '@/assets/placeholder_room.jpg';
 import Button2 from "@/app/components/Button2";
 import Link from "next/link";
-import { Carousel } from 'react-responsive-carousel';
+import {Carousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../../../carousel-custom.css';
 import BedCard from '@/app/components/BedCard';
@@ -38,8 +38,8 @@ interface RoomDetail {
 
 
 const RoomDetails: React.FC = () => {
-    const { id } = useParams();
-    const { dorm } = useParams();
+    const {id} = useParams();
+    const {dorm} = useParams();
     const searchParams = useSearchParams();
     // const year = searchParams.get('year');
     // const gender = searchParams.get('gender');
@@ -48,45 +48,45 @@ const RoomDetails: React.FC = () => {
     const [details, setDetails] = useState<RoomDetail[]>([]);
     const [roomType, setRoomType] = useState<string>('both');
     const [images, setImages] = useState<string[]>([]);
-    const { year, gender } = useFormData();
+    const {year, gender} = useFormData();
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
 
-useEffect(() => {
+    useEffect(() => {
 
-    if (id) {
-        const fetchRoom = async () => {
-            const roomData = await getRoomById(Number(id));
-            setRoom(roomData);
-        };
-        fetchRoom().then(r => r);
+        if (id) {
+            const fetchRoom = async () => {
+                const roomData = await getRoomById(Number(id));
+                setRoom(roomData);
+            };
+            fetchRoom().then(r => r);
 
-        const fetchBeds = async () => {
-            const bedsData = await getBedsByRoomId(Number(id), year);
-            setBeds(bedsData);
-        };
-        fetchBeds().then(r => r);
+            const fetchBeds = async () => {
+                const bedsData = await getBedsByRoomId(Number(id), year);
+                setBeds(bedsData);
+            };
+            fetchBeds().then(r => r);
 
-        const fetchDetails = async () => {
-            const detailsData = await getRoomDetailsByRoomId(Number(id));
-            setDetails(detailsData);
-        };
-        fetchDetails().then(r => r);
+            const fetchDetails = async () => {
+                const detailsData = await getRoomDetailsByRoomId(Number(id));
+                setDetails(detailsData);
+            };
+            fetchDetails().then(r => r);
 
-        const fetchRoomType = async () => {
-            const type = await getRoomType(Number(id), year);
-            setRoomType(type);
-        };
-        fetchRoomType().then(r => r);
+            const fetchRoomType = async () => {
+                const type = await getRoomType(Number(id), year);
+                setRoomType(type);
+            };
+            fetchRoomType().then(r => r);
 
-        const fetchImages = async () => {
-            const imageUrls = await getRoomImages(Number(id));
-            setImages(imageUrls.length > 0 ? imageUrls : Array(5).fill(placeholderImage.src));
-        };
-        fetchImages().then(r => r);
-    }
-}, [id, year]);
+            const fetchImages = async () => {
+                const imageUrls = await getRoomImages(Number(id));
+                setImages(imageUrls.length > 0 ? imageUrls : Array(5).fill(placeholderImage.src));
+            };
+            fetchImages().then(r => r);
+        }
+    }, [id, year]);
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
@@ -114,8 +114,10 @@ useEffect(() => {
 
     return (
         <Layout>
-            <div className="px-0 desktopxl:px-24 medium-desktop:px-48 rounded-3xl gap-2 desktop:gap-10 flex flex-col items-center  mdsuperbook:flex-row mdsuperbook:flex-wrap justify-center">
-                <div className="flex flex-row rounded-xxl bg-cover bg-center w-[350px] h-[230px] tablet:min-h-[520px] tablet:w-[590px]">
+            <div
+                className="px-0 desktopxl:px-24 medium-desktop:px-48 rounded-3xl gap-2 desktop:gap-10 flex flex-col items-center  mdsuperbook:flex-row mdsuperbook:flex-wrap justify-center">
+                <div
+                    className="flex flex-row rounded-xxl bg-cover bg-center w-[350px] h-[230px] tablet:min-h-[520px] tablet:w-[590px]">
                     <Carousel
                         renderArrowPrev={(onClickHandler, hasPrev) =>
                             hasPrev && (
@@ -150,13 +152,14 @@ useEffect(() => {
                                      setIsModalOpen(true);
                                  }}
                             >
-                                <img src={url} alt={`Room image ${index + 1}`} />
+                                <img src={url} alt={`Room image ${index + 1}`}/>
                             </div>
                         ))}
                     </Carousel>
                 </div>
 
-                <div className=" w-[350px] tablet:w-[590px] mdsuperbook:mt-0 mt-5 h-full flex flex-col gap-3 justify-center mdsuperbook:gap-6">
+                <div
+                    className=" w-[350px] tablet:w-[590px] mdsuperbook:mt-0 mt-5 h-full flex flex-col gap-3 justify-center mdsuperbook:gap-6">
 
                     <h1 className="text-3xl  px-[4px] font-bold">
                         {room.name}
@@ -186,13 +189,13 @@ useEffect(() => {
 
                     </div>
                     {/*<YearGenderProvider>*/}
-                        <Link href={`../rooms/${id}/reservation`}>
-                            <div className="flex mt-4 max-mdsuperbook:mt-4 w-full justify-center tablet:justify-start ">
-                                <Button2 className={"w-96 tablet:w-52"}>
-                                    Book a bed
-                                </Button2>
-                            </div>
-                        </Link>
+                    <Link href={`../rooms/${id}/reservation`}>
+                        <div className="flex mt-4 max-mdsuperbook:mt-4 w-full justify-center tablet:justify-start ">
+                            <Button2 className={"w-96 tablet:w-52"}>
+                                Book a bed
+                            </Button2>
+                        </div>
+                    </Link>
                     {/*</YearGenderProvider>*/}
                 </div>
             </div>
@@ -214,7 +217,7 @@ useEffect(() => {
                         <img
                             src={images[selectedImageIndex]}
                             alt={`Room image ${selectedImageIndex + 1}`}
-                            className="w-full max-w-3xl h-auto object-contain" // Изменено: Сделано адаптивным
+                            className="w-full max-w-3xl max-h-screen h-auto object-contain"
                         />
 
                         <button

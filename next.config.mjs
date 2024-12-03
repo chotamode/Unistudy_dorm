c53dad3919d0b01f6c0228d561b0cf29e3b327d9
Eduard <chotamode@gmail.com>
2024-12-04 00:12:10 +0100

    table view in admin, edit room button

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
         REACT_APP_SUPABASE_KEY: process.env.REACT_APP_SUPABASE_KEY,
         CLICKUP_API_KEY: process.env.CLICKUP_API_KEY,
         CLICKUP_CLIENT_ID: process.env.CLICKUP_CLIENT_ID,
+
+        MAKE_WEBHOOK_NEW: process.env.MAKE_WEBHOOK_NEW,
+        MAKE_WEBHOOK_REJECTED: process.env.MAKE_WEBHOOK_REJECTED,
+        MAKE_WEBHOOK_APPROVED: process.env.MAKE_WEBHOOK_APPROVED
     }
 };
 
